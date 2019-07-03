cdc5f9b75e28646f4c9b4a0ac2e85053409dabea
a13576034204 <a13576034204@163.com>
2019-07-03 21:01:43 +0800

    execise3-lecture3 AddMovies

---
+++ Yon/Yon/Controllers/MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Yon.Models;
+using System.Data.Entity;
+
+namespace Yon.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private ApplicationDbContext _context;
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = _context.Movies.Include(c => c.Genre).ToList();
+            return View(movies);
+        }
+        public ActionResult Detail(int id)
+        {
+            var movie = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+            return View(movie);
+        }
+    }
+}
---
+++ Yon/Yon/Migrations/201907030635583_PropogateDataToMovie.cs
@@ -0,0 +1,17 @@
+namespace Yon.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PropogateDataToMovie : DbMigration
+    {
+        public override void Up()
+        {
+
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
