07f2dc451ebfb48747d973ea3c5317d2995a13d2
a13576034204 <a13576034204@163.com>
2019-07-03 12:05:53 +0800

    Lecture 3

--- Yon/Yon/Models/Customer.cs
+++ Yon/Yon/Models/Customer.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Yon.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Yon.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MemberShipType MemberShipType { get; set; }
+        public byte MemberShipTypeId { get; set; }
     }
 }
